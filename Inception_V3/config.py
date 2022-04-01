2df051bac08ad92eb1b4c42da5ee9a1f6ad8fab2
pengjuRen <pengjuRen99@163.com>
2022-04-01 11:05:56 +0800

    Create config.py

---
+++ Inception_V3/config.py
@@ -0,0 +1,14 @@
+# some trainging parameters
+
+EPOCHS = 200
+BATCH_SIZE = 32
+NUM_CLASSES = 400
+
+image_height = 224
+image_width = 224
+channels = 3
+save_model_dir = 'saved_model/model'
+dataset_dir = 'dataset/'
+train_dir = dataset_dir + 'train'
+valid_dir = dataset_dir + 'valid'
+test_dir = dataset_dir + 'test'
